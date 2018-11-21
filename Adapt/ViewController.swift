d5f9956300d52135f9bf86c94fc148284ae7f69d
599944 <samuelodle@c02ww354htdd.local>
2018-11-21 14:42:35 -0500

    Initial Commit

---
+++ Adapt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Adapt
+//
+//  Created by 599944 on 11/21/18.
+//  Copyright © 2018 TheATeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
