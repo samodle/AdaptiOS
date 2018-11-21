ecd5af0f4afa5db67bda786ab8df115730eb1642
SamOdle <sro27@cornell.edu>
2018-11-21 15:52:25 -0500

    starting port

---
+++ Adapt/AdaptLib/Exercise.swift
@@ -0,0 +1,43 @@
+//
+//  Exercise.swift
+//  Adapt
+//
+//  Created by 599944 on 11/21/18.
+//  Copyright © 2018 TheATeam. All rights reserved.
+//
+
+import Foundation
+
+class Exercise
+{
+  //  var List<Gear> MyGear { get; set; } = new List<Gear>();
+    
+    var Name: String?
+    var Description: String?
+    var Instructions: String?
+    
+    var DoIHaveReps: Bool //is this a type of exercise that has a prescribed # of reps?
+    
+    var PrimaryMuscleGroup: Tier1MuscleGroup?
+    var SecondaryMuscleGroup: Tier2MuscleGroup?
+    
+    
+    //constructor
+    init( name: String, descript: String, t1muscle: Tier1MuscleGroup){
+        
+        self.Name = name;
+        self.Description = descript;
+        self.PrimaryMuscleGroup = t1muscle;
+    }
+    /*
+    public Exercise(MisIdiomas l, string name, string description, Tier1Muscle m1)
+{
+    this.Name.Add(name, l);
+    this.Description.Add(description, l);
+    this.PrimaryMuscleGroup = m1;
+    }
+ */
+    
+
+
+}
---
+++ Adapt/Enums.swift
@@ -0,0 +1,24 @@
+//
+//  Enums.swift
+//  Adapt
+//
+//  Created by 599944 on 11/21/18.
+//  Copyright © 2018 TheATeam. All rights reserved.
+//
+
+import Foundation
+
+enum Tier1MuscleGroup{
+    case arms
+    case legs
+    case chest
+    case neck
+    case back
+    case core
+}
+
+enum Tier2MuscleGroup{
+    case biceps
+    case triceps
+    case forearms
+}
